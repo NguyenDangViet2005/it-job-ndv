a88d27e2300b8bcfbb5579572f52044639ea2981
nguyen dang viet <vietnguyen.1022005@gmail.com>
2025-12-26 09:40:53 +0700

    sửa lại project it job

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+import "./blog-highlight.css";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { AuthProvider } from "@/providers/auth.provider";
+import { Toaster } from "sonner";
+
+export const metadata: Metadata = {
+  title: "IT Job",
+  description: "IT Job",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body>
+          <Toaster />
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <AuthProvider>
+
+              {children}
+            </AuthProvider>
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
+  );
+}
