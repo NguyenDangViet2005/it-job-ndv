0ea14aaef1c418e884739f7fa43692e251927cae
nguyen dang viet <vietnguyen.1022005@gmail.com>
2026-02-03 12:53:24 +0700

    thêm cors cho domain vercel

--- src/configs/cors.config.js
+++ src/configs/cors.config.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: "http://localhost:3000", // Chỉ cho phép domain này
+  origin: ["http://localhost:3000", "https://it-job-ndv-d1zf.vercel.app"], // Các domain được phép
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Các phương thức được phép
   credentials: true, // Cho phép gửi cookie/header xác thực nếu cần
   optionsSuccessStatus: 204,
