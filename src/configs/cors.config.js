e9b97935d15d93f3e3c7cef92dc20b33a57d2e8e
nguyen dang viet <vietnguyen.1022005@gmail.com>
2026-02-03 21:33:45 +0700

    change domain vercel again

--- src/configs/cors.config.js
+++ src/configs/cors.config.js
@@ -1,6 +1,9 @@
 export const corsOptions = {
-  origin: ["http://localhost:3000", "https://it-job-ndv.vercel.app/"], // Các domain được phép
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Các phương thức được phép
-  credentials: true, // Cho phép gửi cookie/header xác thực nếu cần
+  origin: [
+    "http://localhost:3000",
+    "https://it-job-ndv.vercel.app"  // Xóa dấu / ở cuối
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
   optionsSuccessStatus: 204,
 };
